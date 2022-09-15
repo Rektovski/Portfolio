09907ccf3c7f611f13b0edff48425fe10ec0b27f
Rektovski <murmanishvili94@gmail.com>
2022-09-15 22:57:34 +0400

    V1.0  Portfolio building

--- src/main/components/AboutMe.js
+++ src/main/components/AboutMe.js
@@ -8,7 +8,7 @@ export default function AboutMe() {
                 Started to learn programming at Softgen LLC. At this moment I have 1 year of experience into
                 the technologies that is written in the "Software Knowledge" section. For the future, I want to
                 learn deeper and deeper to become full-stack developer. I like to work with team or solo.
-                Maybe, we'll the become best partners?
+                Maybe, we'll be the best partners?
             </p>
             <a href={'#contacts'}>
                 Come and get me...
