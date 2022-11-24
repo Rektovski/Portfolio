b6ec30e78577d23ebb9e4ff5fb44170559a15f63
Rektovski <murmanishvili94@gmail.com>
2022-11-25 02:19:45 +0400

    Updated Contact Section Added Mail Sender System

--- src/main/components/Contacts.js
+++ src/main/components/Contacts.js
@@ -1,53 +1,87 @@
 import "../styles/contactsStyle.css";
-import {Card} from "react-bootstrap";
-import {AiFillFacebook as Facebook, AiOutlineCopy as Copy} from "react-icons/ai";
-import {AiFillGithub as GithubIcon} from "react-icons/ai";
+import {Button, Col, Container, Form, Row, Spinner} from "react-bootstrap";
+import emailjs from "emailjs-com";
+import {useRef, useState} from "react";
+import VisitCard from "./VisitCard";
 
 export default function Contacts() {
-    const mail = "murmanishvili94@gmail.com";
+    const [loading, setLoading] = useState(false);
 
-    const copy = async () => {
-        await navigator.clipboard.writeText(mail);
-        window.alert(`Copied!`);
-    }
+    const form = useRef();
+
+    const sendEmail = async (e) => {
+        e.preventDefault();
+        // if form is not filled it will not be sent by default because we use "required"
+        // in all input brackets
+        setLoading(true);
+        await emailjs
+            .sendForm(
+                'service_tmxa0dr',
+                'template_yaeyoff',
+                form.current,
+                'Y-w-i3WhQ1WOF33bv')
+            .then(result => console.log(result.text))
+            .catch(error => console.log(error.text));
+        setLoading(false);
+        e.target.reset();
+    };
 
     return (
-        <div id={"contacts"} className={'contactCard'}>
-            <Card className={'helicopterHelicopter'}>
-                <Card.Header className={'text-center'}>
-                    <Card.Title>
-                        Visit Card
-                    </Card.Title>
-                </Card.Header>
-                <Card.Body>
-                    <div className={'d-flex justify-content-around align-items-center'}>
-                        <div>
-                            {`murmanishvili94@gmail.com`}
+        <Container>
+            <Row className={'d-flex justify-content-around'}>
+                <Col sm={12} md={12} lg={8}>
+                    <Form ref={form} onSubmit={sendEmail} className={'contactForm border rounded m-3 p-3'}>
+                        <div className={''}>
+                            <div className={'me-5'}>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Name:</Form.Label>
+                                    <Form.Control type="text" name={"name"} placeholder="Enter name" required/>
+                                </Form.Group>
+
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Email:</Form.Label>
+                                    <Form.Control type="email" name={"email"} placeholder="Enter email" required/>
+                                </Form.Group>
+
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Subject:</Form.Label>
+                                    <Form.Control type="text" name={"subject"} placeholder="Enter subject" required/>
+                                </Form.Group>
+                            </div>
+
+                            <div>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Text:</Form.Label>
+                                    <Form.Control className={'formTextarea'} type={"textarea"} as={"textarea"}
+                                                  name={"text"} placeholder="Enter text" required/>
+                                </Form.Group>
+                            </div>
                         </div>
-                        <Copy size={50} className={'copy-icon mx-4'} onClick={() => {copy()}}/>
-                    </div>
-                    <div className={'d-flex justify-content-around align-items-center my-3'}>
-                        <a
-                            href={'https://www.facebook.com/OtexRektovski/'}
-                            target={"_blank"}
-                            rel={"noreferrer"}
-                        >
-                            <Facebook size={50}
-                                      className={'social-icons1'}
-                            />
-                        </a>
-                        <a
-                            href={'https://github.com/Rektovski'}
-                            target={"_blank"}
-                            rel={"noreferrer"}
-                        >
-                            <GithubIcon size={50}
-                                        className={'social-icons2'}
-                            />
-                        </a>
-                    </div>
-                </Card.Body>
-            </Card>
-        </div>
+
+                        <div className={'d-flex'}>
+                            <div className={'flex-fill'}></div>
+
+
+
+                            {
+                                loading ?
+                                    <div className={"d-flex align-items-center"}>
+                                        <Spinner style={{color: "lightgray"}} animation="grow" role="status"></Spinner>
+                                        <span className="">Loading...</span>
+                                    </div>
+                                    :
+                                    <Button variant="primary" type="submit" value={"send"}>
+                                        Submit
+                                    </Button>
+                            }
+                        </div>
+                    </Form>
+                </Col>
+
+                <Col className={'d-flex align-items-center justify-content-center'}>
+                    <VisitCard/>
+                </Col>
+            </Row>
+        </Container>
     )
 }
---
+++ src/main/components/VisitCard.js
@@ -0,0 +1,62 @@
+import {AiFillFacebook as Facebook,
+    AiFillGithub as GithubIcon,
+    // AiOutlineCopy as Copy,
+    AiFillLinkedin as LinkedIn
+} from "react-icons/ai";
+import "../styles/visitCardStyle.css";
+
+export default function VisitCard() {
+    // const mail = "murmanishvili94@gmail.com";
+    //
+    // const copy = async () => {
+    //     await navigator.clipboard.writeText(mail);
+    //     window.alert(`Copied!`);
+    // }
+
+    return (
+        <>
+            <div id={"contacts"} className={'contactCard'}>
+                <div className={'helicopterHelicopter'}>
+                    <h4 className={'text-center font-monospace'}>Visit Card</h4>
+
+                    {/*<div className={'d-flex justify-content-around align-items-center'}>*/}
+                    {/*    <div>*/}
+                    {/*        {`murmanishvili94@gmail.com`}*/}
+                    {/*    </div>*/}
+                    {/*    <Copy size={50} className={'copy-icon mx-4'} onClick={() => {copy()}}/>*/}
+                    {/*</div>*/}
+
+                    <div className={'d-flex justify-content-around align-items-center my-3'}>
+                        <a
+                            href={'https://www.facebook.com/OtexRektovski/'}
+                            target={"_blank"}
+                            rel={"noreferrer"}
+                        >
+                            <Facebook size={50}
+                                      className={'social-icons1'}
+                            />
+                        </a>
+                        <a
+                            href={'https://github.com/Rektovski'}
+                            target={"_blank"}
+                            rel={"noreferrer"}
+                        >
+                            <GithubIcon size={50}
+                                        className={'social-icons2'}
+                            />
+                        </a>
+                        <a
+                            href={'https://www.facebook.com/OtexRektovski/'}
+                            target={"_blank"}
+                            rel={"noreferrer"}
+                        >
+                            <LinkedIn size={50}
+                                      className={'social-icons1'}
+                            />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
