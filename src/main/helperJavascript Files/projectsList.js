1b27de503337ac3ceb0e39ebee605bfd659bb021
Rektovski <murmanishvili94@gmail.com>
2022-12-04 02:24:37 +0400

    Every update after this will be adding new projects.

--- src/main/helperJavascript Files/projectsList.js
+++ src/main/helperJavascript Files/projectsList.js
@@ -8,42 +8,42 @@ import lawyer from "../images/lawyer.png";
 import legion from "../images/legion.png";
 
 export const projectsList = [
-    {
-        "name": "gesa",
-        "link": "https://geuropeanstudeisassosication.netlify.app/",
-        "image": gesa,
-        "alt": "foto of project"
-    },
-    {
-        "name": "marksman",
-        "link": "https://marksmangeorgia.netlify.app/",
-        "image": marksman,
-        "alt": "foto of project"
-    },
-    {
-        "name": "saunje",
-        "link": "https://saunje.netlify.app/",
-        "image": saunje,
-        "alt": "foto of project"
-    },
-    {
-        "name": "Movies API",
-        "link": "https://filmebi.netlify.app/",
-        "image": movies,
-        "alt": "foto of project"
-    },
+    // {
+    //     "name": "gesa",
+    //     "link": "https://geuropeanstudeisassosication.netlify.app/",
+    //     "image": gesa,
+    //     "alt": "foto of project"
+    // },
+    // {
+    //     "name": "marksman",
+    //     "link": "https://marksmangeorgia.netlify.app/",
+    //     "image": marksman,
+    //     "alt": "foto of project"
+    // },
+    // {
+    //     "name": "saunje",
+    //     "link": "https://saunje.netlify.app/",
+    //     "image": saunje,
+    //     "alt": "foto of project"
+    // },
+    // {
+    //     "name": "Movies API",
+    //     "link": "https://filmebi.netlify.app/",
+    //     "image": movies,
+    //     "alt": "foto of project"
+    // },
     {
         "name": "Weather API",
         "link": "https://weatherotara.netlify.app/",
         "image": weather,
         "alt": "foto of project"
     },
-    {
-        "name": "Shop API",
-        "link": "https://shopotara.netlify.app/",
-        "image": shop,
-        "alt": "foto of project"
-    },
+    // {
+    //     "name": "Shop API",
+    //     "link": "https://shopotara.netlify.app/",
+    //     "image": shop,
+    //     "alt": "foto of project"
+    // },
     {
         "name": "Lawyer Project",
         "link": "https://lawyerotara.netlify.app/",
