91a7c312db53a8eb22e15796a8145acf4160e692
Rektovski <murmanishvili94@gmail.com>
2022-12-06 04:37:05 +0400

    Every update after this will be adding new projects.

--- src/main/components/profile/HeroInfo.js
+++ src/main/components/profile/HeroInfo.js
@@ -3,8 +3,18 @@ import {CardImg, Col, Container, Row} from "react-bootstrap";
 import {knowledgeArray} from "../../helperJavascript Files/knowledgeArray";
 import {Card, CardBody, CardFooter} from "@chakra-ui/react";
 import profileGif from "../../images/background.webp";
+import {RiCheckboxBlankLine as RectIcon} from "react-icons/ri";
+import {
+    AiFillCloseCircle as CloseIcon,
+    AiOutlineMinus as MinusIcon,
+    AiOutlineFolderAdd as TerminalAddIcon
+} from "react-icons/ai";
+import {FaRegHandPeace as HelloIcon} from "react-icons/fa";
+import {useState} from "react";
 
 export default function HeroInfo() {
+    const [closeButtonTextShow, setCloseButtonTextShow] = useState(false);
+
     return (
         <>
             <div className={'Header'}>
@@ -12,14 +22,48 @@ export default function HeroInfo() {
                     <img src={profileGif} alt={'profileGif'}/>
                 </div>
                 <div className={'HeaderText m-2'}>
-                    <div className={'bg-dark p-1 d-flex justify-content-between'}>
-                        <div></div>
+
+                    <div className={'bg-dark p-1 d-flex align-items-center justify-content-between'}>
+                        <div className={'p-2 me-5'}><TerminalAddIcon size={20}/></div>
                         <div className={''}>
                             user@host
                         </div>
-                        <div>X</div>
+                        <div className={'d-flex justify-content-center align-items-center'}>
+                            <MinusIcon className={'me-3'}/>
+                            <RectIcon className={'HeaderRectangle me-3'}/>
+                            <CloseIcon
+                                onMouseOver={() => {
+                                    if (!closeButtonTextShow) setCloseButtonTextShow(true);
+                                    console.log('1')
+                                }}
+                                className={'closeButton me-1'}
+                            />
+
+                        </div>
                     </div>
+
                     <div className={'HeaderHello'}>
+                        <div>
+                            <span className={"HeaderText1"}>user@host:</span>
+                            <span className={"HeaderText2"}>~</span>
+                            <span className={"HeaderText3"}>$</span>
+                        </div>
+                        <div>
+                            <span className={"HeaderText1"}>user@host:</span>
+                            <span className={"HeaderText2"}>~</span>
+                            <span className={"HeaderText3"}>$</span>
+                        </div>
+                        <div>
+                            <span className={"HeaderText1"}>user@host:</span>
+                            <span className={"HeaderText2"}>~</span>
+                            <span className={"HeaderText3"}>$</span>
+                        </div>
+                        <div className={' align-items-center'}>
+                            <span className={"HeaderText1"}>user@host:</span>
+                            <span className={"HeaderText2"}>~</span>
+                            <span className={"HeaderText3"}>$</span> Sudo emoji!
+                            <div className={'d-flex'}><HelloIcon/>..</div>
+                        </div>
                         <div>
                             <span className={"HeaderText1"}>user@host:</span>
                             <span className={"HeaderText2"}>~</span>
@@ -35,6 +79,16 @@ export default function HeroInfo() {
                             <span className={"HeaderText2"}>~</span>
                             <span className={"HeaderText3"}>$</span> Here are my skills and achievements
                         </div>
+                        {
+                            closeButtonTextShow ?
+                                <div>
+                                    <span className={"HeaderText1"}>user@host:</span>
+                                    <span className={"HeaderText2"}>~</span>
+                                    <span className={"HeaderText3"}>$</span> Well, pardon me young man, excuse the shit
+                                    out of my goddamn French but did you just threaten me!?
+                                </div>
+                                : ""
+                        }
                     </div>
                 </div>
             </div>
@@ -49,7 +103,8 @@ export default function HeroInfo() {
                             <Col key={id} sm={6} md={4} lg={3} xl={2} xxl={2}>
                                 <Card align='center' className={'text-light KnowledgeCard'}>
                                     <CardBody>
-                                        <CardImg className={'rounded'} style={{width: 70}} src={item.image} alt={'tech logos'}/>
+                                        <CardImg className={'rounded'} style={{width: 70}} src={item.image}
+                                                 alt={'tech logos'}/>
                                     </CardBody>
                                     <CardFooter className={'text-center'}>
                                         {item.name}
