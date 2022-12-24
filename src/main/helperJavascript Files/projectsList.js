cd19afef38412d42c12d33986e37d34ec356cd53
Rektovski <murmanishvili94@gmail.com>
2022-12-24 21:16:22 +0400

    Every update after this will be adding new projects.

--- src/main/helperJavascript Files/projectsList.js
+++ src/main/helperJavascript Files/projectsList.js
@@ -1,31 +1,17 @@
 import gesa from "../assets/projectImages/gesa.png";
-import movies from "../assets/projectImages/movies.png";
-import saunje from "../assets/projectImages/saunje.png";
-import marksman from "../assets/projectImages/marksman.png";
+// import saunje from "../assets/projectImages/saunje.png";
 import weather from "../assets/projectImages/weather.png";
-import shop from "../assets/projectImages/shop.png";
 import lawyer from "../assets/projectImages/lawyer.png";
 import legion from "../assets/projectImages/legion.png";
 import bitcamp from "../assets/projectImages/bitcamp.png";
 import clotheshop from "../assets/projectImages/clotheshop.png";
+import lawyerHub from "../assets/projectImages/lawyerHub.png";
 
 export const projectsList = [
-    // {
-    //     "name": "marksman",
-    //     "link": "https://marksmangeorgia.netlify.app/",
-    //     "image": marksman,
-    //     "alt": "foto of project"
-    // },
     // {
     //     "name": "saunje",
     //     "link": "https://saunje.netlify.app/",
     //     "image": saunje,
-    //     "alt": "foto of project"
-    // },
-    // {
-    //     "name": "Movies API",
-    //     "link": "https://filmebi.netlify.app/",
-    //     "image": movies,
     //     "alt": "foto of project"
     // },
     {
@@ -40,12 +26,6 @@ export const projectsList = [
         "image": gesa,
         "alt": "foto of project"
     },
-    // {
-    //     "name": "Shop API",
-    //     "link": "https://shopotara.netlify.app/",
-    //     "image": shop,
-    //     "alt": "foto of project"
-    // },
     {
         "name": "Lawyer Project",
         "link": "https://lawyerotara.netlify.app/",
@@ -68,6 +48,12 @@ export const projectsList = [
         "name": "Clothes Shop",
         "link": "https://clotheshopotara.netlify.app/",
         "image": clotheshop,
+        "alt": "foto of project"
+    },
+    {
+        "name": "Laywer Hub",
+        "link": "https://laywerhub.netlify.app/",
+        "image": lawyerHub,
         "alt": "foto of project"
     },
 ]
